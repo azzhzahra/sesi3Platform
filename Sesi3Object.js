4c33306ece87069d87844b02504a728204df7a41
azzhzahra <114793003+azzhzahra@users.noreply.github.com>
2023-10-15 07:56:49 +0700

    Add files via upload

---
+++ Sesi3Object.js
@@ -0,0 +1,16 @@
+// Bertipe Object
+
+const restaurant = {
+  name: "LA",
+  city: "Sukabumi",
+  favoriteDrink: "Coffe",
+  favoriteFood: "Smashed Chicken",
+  isVegan: false,
+};
+
+const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+console.log("Nama : " + name);
+console.log("Kota : " + city);
+console.log("Minuman Favorit : " + favoriteDrink);
+console.log("Makanan Favorit : " + favoriteFood);
+console.log("Vegan : " + isVegan);
